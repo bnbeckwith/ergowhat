03d90d07c83e64eb588fe2e6b382451daa226967
Ben Beckwith <ben@mutuallyhuman.com>
2017-04-25 15:52:59 -0400

    Add all key possibilities

--- src/types.rs
+++ src/types.rs
@@ -9,12 +9,32 @@ pub enum Key {
 
 #[derive(Debug, PartialEq)]
 pub enum Action{
+    // Run a specific function
     Function(Key),
+    // Tappable function
     FunctionTap(Key),
+    // Sets a layer that is always valid
+    DefaultLayerSet(u64),
+    // Turn on/off layer only
     LayerSet(u64, String),
+    // Turn on layer onlyl and clea all layers on release
+    LayerSetClear(u64),
+    // Momentary layer setting
     LayerMomentary(u64),
+    // Turns on momentary `layer` while holding, but key if tapping
     LayerTapKey(u64, Key),
+    // Turn on layer momentarily and toggles it on taps
+    LayerTapToggle(u64),
+    // Toggle setting of layer
+    LayerToggle(u64),
+    // Usually of the form (KEY | KEY)
+    // to press multiple
+    Mods(Key),
+    // Run these two keys together
+    // The usual way is (modifier, key)
     ModsKey(Key, Key),
+    // Modifier while holding, key if tapping
+    // (Mod, Key)
     ModsTapKey(Key, Key)
 }
 
